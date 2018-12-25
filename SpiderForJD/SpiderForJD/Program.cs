9121817fbc6baa89346de9a07e464cf9cc9a6cd6
jackking2015 <154661793@qq.com>
2018-12-25 16:59:53 +0800

    整理HTTPHelper

---
+++ SpiderForJD/SpiderForJD/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiderForJD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpHelper helper = new HttpHelper();
+            HttpItem item = new HttpItem();
+            item.Url = "https://www.jd.com/";
+            HttpResult result = new HttpResult();
+            result = helper.GetHtml(item);
+            //Console.WriteLine(result.Cookie);
+            Console.WriteLine(result.Header);
+            //Console.WriteLine(result.Html);
+            Console.WriteLine(result.StatusCode);
+        }
+    }
+}
